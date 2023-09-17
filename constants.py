3d5fd66ea2a31c9cae39f669331335354c6861b8
Blytz <Ruben.costa.nt@gmail.com>
2023-09-17 09:11:18 +0100

    Integrated a constants.py file with main.py for maintainability and readability

---
+++ constants.py
@@ -0,0 +1,45 @@
+REQUIRED_ROLES = ['Owner', 'Headadmin']
+
+CHANNELS = {
+    "rules": 1144340352224985189,          # channel ID for "rules"
+    "community": 1150112197234671736,      # channel ID for "community"
+    "role_selection": 1144339896639697026  # channel ID for "role_selection"
+}
+
+
+GENDER_ROLE_EMOJIS = {
+    '<:hehim:1150102718254415944>': "He/Him",
+    '<:sheher:1150102749460041748>': "She/Her",
+    '<:theythem:1150102766056906804>': "They/Them",
+    '<:itits:1150102803797266493>': "It/Its",
+    '<:any_pronoun:1150102835627839519>': "Any Pronoun"
+    
+}
+
+PLATFORM_ROLE_EMOJIS = {
+    '<:pc:1150072742742343720>': 'PC',
+    '<:playstation:1150072803027062855>': 'Playstation',
+    '<:xbox:1150072857674666136>': 'Xbox',
+    '<:nintendo:1150072876544827462>': 'Nintendo',
+    '<:mobile:1150073175556767815>': 'Mobile',
+    
+}
+
+SERVER_ROLE_EMOJIS = {
+    '📕': 'Rule Changes',
+    '🚨': 'Server Restarts',
+    '📌': 'Event Announcements',
+    '📢': 'Community Announcements',
+    
+}
+
+GENERAL_COMMANDS = [
+        "/announce: Send an announcement to a specific channel",
+        "/post: Post content to a specific channel",
+        "/chooseyourgender: Display a message for users to select their roles using reactions",
+        "/chooseyourplatform: Display a message for users to select their roles using reactions",
+        "/chooseyourserverroles: Display a message for users to select their roles using reactions",
+        "/playerid: Query the Discord user associated with a player ID or vice versa",
+        "/alderonid: Link your Discord Account to your AlderonID ",
+        "/clear: Clear a specified number of messages from a channel"
+    ]
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from decouple import config
 from discord.ext import commands
 from dotenv import load_dotenv
 from trello import add_strike_to_trello, move_card_to_list, update_card_description, search_for_card, TRELLO_LIST_ID
+from constants import REQUIRED_ROLES, CHANNELS, GENDER_ROLE_EMOJIS, PLATFORM_ROLE_EMOJIS, SERVER_ROLE_EMOJIS, GENERAL_COMMANDS
 
 conn = sqlite3.connect('players.db')
 c = conn.cursor()
@@ -29,44 +30,6 @@ STRIKE_LIST_MAPPING_STR = os.getenv('STRIKE_LIST_MAPPING')
 STRIKE_LIST_MAPPING = json.loads(STRIKE_LIST_MAPPING_STR)
 STRIKE_STAGE_STR = config('STRIKE_STAGE')
 STRIKE_STAGE = json.loads(STRIKE_STAGE_STR)
-
-
-
-REQUIRED_ROLES = ['Owner', 'Headadmin']
-
-CHANNELS = {
-    "rules": 1144340352224985189,          # channel ID for "rules"
-    "community": 1150112197234671736,      # channel ID for "community"
-    "role_selection": 1144339896639697026  # channel ID for "role_selection"
-}
-
-
-GENDER_ROLE_EMOJIS = {
-    '<:hehim:1150102718254415944>': "He/Him",
-    '<:sheher:1150102749460041748>': "She/Her",
-    '<:theythem:1150102766056906804>': "They/Them",
-    '<:itits:1150102803797266493>': "It/Its",
-    '<:any_pronoun:1150102835627839519>': "Any Pronoun"
-    
-}
-
-PLATFORM_ROLE_EMOJIS = {
-    '<:pc:1150072742742343720>': 'PC',
-    '<:playstation:1150072803027062855>': 'Playstation',
-    '<:xbox:1150072857674666136>': 'Xbox',
-    '<:nintendo:1150072876544827462>': 'Nintendo',
-    '<:mobile:1150073175556767815>': 'Mobile',
-    
-}
-
-SERVER_ROLE_EMOJIS = {
-    '📕': 'Rule Changes',
-    '🚨': 'Server Restarts',
-    '📌': 'Event Announcements',
-    '📢': 'Community Announcements',
-    
-}
-
 
 
 intents = discord.Intents.default()
@@ -549,23 +512,12 @@ async def _commands(interaction: discord.Interaction):
     if interaction.user.bot:
         return
 
-    
-    general_commands = [
-        "/announce: Send an announcement to a specific channel",
-        "/post: Post content to a specific channel",
-        "/chooseyourgender: Display a message for users to select their roles using reactions",
-        "/chooseyourplatform: Display a message for users to select their roles using reactions",
-        "/chooseyourserverroles: Display a message for users to select their roles using reactions",
-        "/playerid: Query the Discord user associated with a player ID or vice versa",
-        "/alderonid: Link your Discord Account to your AlderonID ",
-        "/clear: Clear a specified number of messages from a channel"
-    ]
 
     # Creates an embed with a title and description
     embed = discord.Embed(title="Help", description="Here's a list of my commands:", color=0x00ff00)
     
     # Addd the general commands to the embed
-    embed.add_field(name="ℹ️ General", value="\n".join(general_commands), inline=False)
+    embed.add_field(name="ℹ️ General", value="\n".join(GENERAL_COMMANDS), inline=False)
 
     # Sends the embed as an ephemeral message in response to the interaction
     await interaction.response.send_message(embed=embed, ephemeral=True)
